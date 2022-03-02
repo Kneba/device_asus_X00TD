2ec68c6cf77eaa21545eedd745885a8879a75b8f
Kneba <abenkenary3@gmail.com>
2022-03-03 02:48:03 +0700

    X00TD: Initial PEX

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -9,8 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common POSP stuff
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+# Inherit some common PEX stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from X00TD device
