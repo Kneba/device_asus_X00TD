f768cf98dea4709c1d0e1ef2edd257f55386ad71
Kneba <abenkenary3@gmail.com>
2022-04-28 17:30:21 +0700

    X00TD: Update gapps and coral FP

--- potato_X00TD.mk
+++ potato_X00TD.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Inherit some common POSP stuff
 $(call inherit-product, vendor/potato/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +29,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-asus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=ASUS_X00T \
     PRODUCT_NAME=WW_X00T \
-    PRIVATE_BUILD_DESC="coral-user 12 SQ1A.220205.002 8010174 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 12 SP2A.220405.003 8210211 release-keys"
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/coral/coral:12/SQ1A.220205.002/8010174:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:12/SP2A.220405.003/8210211:user/release-keys"
